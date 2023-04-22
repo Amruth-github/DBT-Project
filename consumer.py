2e50008664c7f1794b93ee17f73557d6eee8d6f5
Amruth-github <79594127+Amruth-github@users.noreply.github.com>
2023-04-22 16:51:47 +0530

    Add files via upload

--- consumer.py
+++ consumer.py
@@ -4,6 +4,17 @@ from pyspark.sql import SparkSession
 from flask import Flask, render_template
 from threading import *
 import pandas as pd
+import pymongo
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["Kafka"]
+mycol = mydb["Batch_results"]
+
 schema = StructType([
     StructField("id", StringType(), True),
     StructField("sentiment", StringType(), True),
@@ -13,18 +24,21 @@ spark = SparkSession \
     .builder \
     .appName("KafkaStreaming") \
     .getOrCreate()
-   
+
 spark.sparkContext.setLogLevel("WARN")
 
+
+sentiment_dict = {"positive": 0, "negative": 0}
+
 df = spark.readStream \
-         .format("kafka") \
-         .option("kafka.bootstrap.servers", "localhost:9092") \
-         .option("subscribe", "sentiment") \
-         .option("startingOffsets", "earliest") \
-         .load() \
-         .selectExpr("CAST(value AS STRING)") \
-         .select(from_json(col("value"), schema).alias("data")) \
-         .select("data.*")
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "sentiment") \
+    .option("startingOffsets", "earliest") \
+    .load() \
+    .selectExpr("CAST(value AS STRING)") \
+    .select(from_json(col("value"), schema).alias("data")) \
+    .select("data.*")
 
 
 query = df.writeStream \
@@ -32,22 +46,30 @@ query = df.writeStream \
           .outputMode("append") \
           .start()
 
-print(query);
-          
-          
+print(query)
+
+
 sentiment_count = dict()
+
+
 def process_batch(batch_df, batch_id):
     # Get the sentiment count as a dictionary
     global sentiment_count
     sentiment_count = (
-        batch_df.groupBy('hashtag')
+        batch_df.groupBy('sentiment')
         .count()
-        .rdd.map(lambda x: (x['hashtag'], x['count']))
+        .rdd.map(lambda x: (x['sentiment'], x['count']))
         .collectAsMap()
     )
-    print(sentiment_count);
-   
-
+    for i in sentiment_count.keys():
+        if i == "0":
+            sentiment_dict["positive"] += int(sentiment_count[i])
+        else:
+            sentiment_dict["negative"] += int(sentiment_count[i])
+        to_insert = dict(sentiment_count)
+        to_insert["Batch-id"] = batch_id
+        mycol.insert_one(to_insert)
+    
 query_sentiment = (
     df.writeStream
     .foreachBatch(process_batch)
