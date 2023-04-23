49acdc1d4dc2a30be59df5d55f79709573d9499f
Akarsh Jayanth V Shettigar <76930147+Akarsh-J@users.noreply.github.com>
2023-04-23 11:20:01 +0530

    Included aggregation of data for stream processing

--- consumer.py
+++ consumer.py
@@ -1,83 +1,51 @@
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 from pyspark.sql import SparkSession
-from flask import Flask, render_template
-from threading import *
-import pandas as pd
-import pymongo
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["Kafka"]
-mycol = mydb["Batch_results"]
-
-schema = StructType([
-    StructField("id", StringType(), True),
-    StructField("sentiment", StringType(), True),
-    StructField("hashtag", StringType(), True)
-])
-spark = SparkSession \
-    .builder \
-    .appName("KafkaStreaming") \
-    .getOrCreate()
-
+spark = SparkSession.builder.appName("Traffic").getOrCreate()
 spark.sparkContext.setLogLevel("WARN")
 
+schema = StructType([
+    StructField("id",StringType(), True),
+    StructField("datetime",StringType(), True),
+    StructField("issue",StringType(), True)
+])
 
-sentiment_dict = {"positive": 0, "negative": 0}
+aggregate_count = {"Traffic Hazard":0, "Crash Urgent": 0, "Crash Service": 0, "COLLISION": 0}
 
-df = spark.readStream \
-    .format("kafka") \
+def update_issue_counts(df, epoch_id):
+    # Aggregate the counts for each issue
+    counts = df.groupBy("issue").count().collect()
+    for row in counts:
+        issue = row["issue"]
+        count = row["count"]
+        aggregate_count[issue] += count
+    # Print the updated counts
+    print(aggregate_count)
+
+
+df = spark.readStream.format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "sentiment") \
-    .option("startingOffsets", "earliest") \
-    .load() \
-    .selectExpr("CAST(value AS STRING)") \
-    .select(from_json(col("value"), schema).alias("data")) \
-    .select("data.*")
+    .option("subscribe","TrafficHazard,CrashUrgent,CrashService,COLLISION") \
+    .option("startingOffsets", "latest") \
+    .option("failOnDataLoss", "false") \
+    .load()
 
+raw_df = df.selectExpr("CAST(value AS STRING)")
 
-query = df.writeStream \
-          .format("console") \
-          .outputMode("append") \
-          .start()
+s_df = raw_df.select(from_json(raw_df.value, schema).alias("data"))
 
-print(query)
+traffic_df = s_df.select(col("data.id"),col("data.datetime"),col("data.issue"))
 
+output_df = traffic_df.select(to_json(struct(col("*"))).alias("value"))
 
-sentiment_count = dict()
+op = traffic_df.writeStream.format("console").foreachBatch(update_issue_counts).outputMode("append").start()
 
-
-def process_batch(batch_df, batch_id):
-    # Get the sentiment count as a dictionary
-    global sentiment_count
-    sentiment_count = (
-        batch_df.groupBy('sentiment')
-        .count()
-        .rdd.map(lambda x: (x['sentiment'], x['count']))
-        .collectAsMap()
-    )
-    for i in sentiment_count.keys():
-        if i == "0":
-            sentiment_dict["positive"] += int(sentiment_count[i])
-        else:
-            sentiment_dict["negative"] += int(sentiment_count[i])
-        to_insert = dict(sentiment_count)
-        to_insert["Batch-id"] = batch_id
-        mycol.insert_one(to_insert)
-    
-query_sentiment = (
-    df.writeStream
-    .foreachBatch(process_batch)
+query = output_df.writeStream.format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("checkpointLocation", "/tmp/spark_checkpoint") \
+    .option("topic", "output") \
+    .outputMode("update") \
     .start()
-)
-
-query_sentiment.awaitTermination()
 
 query.awaitTermination()
-spark.stop()
-
